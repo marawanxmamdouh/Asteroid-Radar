7f212727ed4352cedd7e7e96523d21d14bc71db3
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-14 12:53:10 +0200

    Initialize project an add dependencies.

---
+++ app/src/main/java/dev/marawanxmamdouh/asteroidradar/main/MainFragment.kt
@@ -0,0 +1,45 @@
+package dev.marawanxmamdouh.asteroidradar.main
+
+import android.os.Bundle
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import dev.marawanxmamdouh.asteroidradar.R
+import dev.marawanxmamdouh.asteroidradar.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentMainBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        binding.viewModel = viewModel
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.main_overflow_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+}
---
+++ app/src/main/java/dev/marawanxmamdouh/asteroidradar/main/MainViewModel.kt
@@ -0,0 +1,5 @@
+package dev.marawanxmamdouh.asteroidradar.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel()
