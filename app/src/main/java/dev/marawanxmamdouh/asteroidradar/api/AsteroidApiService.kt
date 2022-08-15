045c8afc5ecdf8722ebf24b0953f73409ae3ae83
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-15 14:51:08 +0200

    Update Timeout to 30 sec

--- app/src/main/java/dev/marawanxmamdouh/asteroidradar/api/AsteroidApiService.kt
+++ app/src/main/java/dev/marawanxmamdouh/asteroidradar/api/AsteroidApiService.kt
@@ -9,9 +9,9 @@ import retrofit2.http.Query
 import java.util.concurrent.TimeUnit
 
 private val okHttpClient = OkHttpClient.Builder()
-    .connectTimeout(10, TimeUnit.SECONDS)
-    .writeTimeout(10, TimeUnit.SECONDS)
-    .readTimeout(10, TimeUnit.SECONDS)
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .writeTimeout(30, TimeUnit.SECONDS)
+    .readTimeout(30, TimeUnit.SECONDS)
     .build()
 
 private val retrofit = Retrofit.Builder()
