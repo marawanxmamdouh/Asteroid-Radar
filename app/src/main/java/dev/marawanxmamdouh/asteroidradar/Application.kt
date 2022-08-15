dba37325eb5017e0ce48809da798a41e721e2511
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-15 02:44:20 +0200

    Add worker
    downloads and saves today's asteroids in background once a day when the device is charging and wifi is enabled.

---
+++ app/src/main/java/dev/marawanxmamdouh/asteroidradar/Application.kt
@@ -0,0 +1,41 @@
+package dev.marawanxmamdouh.asteroidradar
+
+import android.app.Application
+import androidx.work.*
+import dev.marawanxmamdouh.asteroidradar.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class Application : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    private fun delayedInit() = applicationScope.launch {
+        setupRecurringWork()
+    }
+
+    private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresCharging(true)
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+    }
+}
---
+++ app/src/main/java/dev/marawanxmamdouh/asteroidradar/work/RefreshDataWorker.kt
@@ -0,0 +1,29 @@
+package dev.marawanxmamdouh.asteroidradar.work
+
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import dev.marawanxmamdouh.asteroidradar.database.getDatabase
+import dev.marawanxmamdouh.asteroidradar.repository.AsteroidRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = AsteroidRepository(database)
+
+        return try {
+            repository.refreshAsteroids()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+}
